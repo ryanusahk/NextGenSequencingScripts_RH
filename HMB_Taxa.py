c01ba358ceaec55098e4d06d983346cfcdbaf68a
Ryan Hsu <ryanusahk@gmail.com>
2015-11-23 21:21:18 -0800

    Updated HMB_Taxa to detect phasing and remove it prior to alignment with usearch

--- HMB_Taxa.py
+++ HMB_Taxa.py
@@ -8,15 +8,25 @@ import sys
 from os.path import expanduser
 import json
 
+
+### FILE PATHS ###
 ASSEMBLED_DIRECTORY = sys.argv[1]
 HMB_DATABASE = sys.argv[2]
 BLAST6OUTPUT = 'blast6/temp.blast6'
+FILTERED_DIRECTORY = 'filtered/'
 TALLY_TXT = 'tally.txt'
 TALLY_HMB = 'tally.hmb'
+REPORT_TXT = 'report.txt'
 PATH_TO_USEARCH8 = '/bin/usearch8'
+
+### FILTER OPTIONS ###
+MIN_LENGTH = 300
+PHASING_ANNEAL = "ACTCCTACGGGAGGCAGCAGTG"
 
 if not os.path.exists('blast6'):
     os.makedirs('blast6')
+if not os.path.exists('filtered'):
+	os.makedirs('filtered')
 
 # Create Log File
 sample_dictionary = dict()
@@ -24,6 +34,34 @@ organism_database = []
 
 
 # http://atacama.qb3.berkeley.edu/auto/sahara/namib/home/ryanhsu/bin/pear
+
+def filterFastQ(assembledFile):
+	def getSequences(FASTQfile):
+		tempFile = open(ASSEMBLED_DIRECTORY + FASTQfile).read().split('\n')
+		sequences = []
+		lineNum = 0
+		for line in tempFile:
+			if lineNum == 1:
+				sequences.append(line)
+			lineNum += 1
+			if lineNum == 4:
+				lineNum = 0
+		return sequences
+
+	conditionName = assembledFile.split(".")[0]
+	newFileName = conditionName + "." + "filtered.fasta"
+
+	seqs = getSequences(assembledFile)
+	filteredFastQ = ""
+	for seqNum in range(len(seqs)):
+		seq = seqs[seqNum]
+		if len(seq) > MIN_LENGTH:
+			phasingIndex = seq.find(PHASING_ANNEAL, 0, 36)
+			if phasingIndex >= 0:
+				filteredSeq = seq[phasingIndex:]
+				filteredFastQ += ">Sequence" + str(seqNum) + '\n' + filteredSeq + '\n'
+	open(FILTERED_DIRECTORY + newFileName, 'w').write(filteredFastQ)
+	
 
 def getAllOrganisms(database):
 	arrayoforgs = []
@@ -58,8 +96,8 @@ def usearch(assembledFile):
 
 	process = subprocess.Popen(args, stdout=subprocess.PIPE)
 
-	for line in iter(process.stdout.readline, ''):
-		sys.stdout.write(line)
+	# for line in iter(process.stdout.readline, ''):
+	# 	sys.stdout.write(line)
 
 	process.wait()
 
@@ -70,13 +108,20 @@ def findAssembledFASTQ(directoryList):
 			assembledFiles.append(f)
 	return sorted(assembledFiles)
 
+def findFilteredFASTA(directoryList):
+	filteredFiles = []
+	for f in directoryList:
+		if f.split('.')[-2] == 'filtered' and f.split('.')[-1] == 'fasta':
+			filteredFiles.append(f)
+	return sorted(filteredFiles)
+
 def saveTallyHMB():
 	jsonOutput = json.dumps(sample_dictionary)
 	print 'Saving ' + TALLY_HMB
 	open(TALLY_HMB, 'w').write(jsonOutput)
 
 def saveTallyText():
-	tallyText = 'sample'
+	tallyText = 'sample  '
 	for org in organism_database:
 		tallyText += '\t' + org
 
@@ -94,6 +139,33 @@ def saveTallyText():
 	print 'Saving ' + TALLY_TXT
 	open(TALLY_TXT, 'w').write(tallyText)
 
+def saveReport():
+	reportText = 'sample\tReads\tOrganisms Over 5%'
+
+	plateSet = 'HMB'
+	for platenum in range(1,13):
+		for letter in 'ABCDEFGH':
+			for column in range(1,13):
+				sampleID = argsToSampleID(plateSet, platenum, letter, column)
+				if sampleID in sample_dictionary:
+					reportText += '\n'
+					reportText += sampleID
+
+					totalReads = 0
+					for org in organism_database:
+						totalReads += sample_dictionary[sampleID][org]
+					reportText += '\t' + str(totalReads)
+
+					threshold = 0.05*totalReads
+
+					for org in organism_database:
+						if sample_dictionary[sampleID][org] > threshold:
+							reportText += '\t' + org
+
+
+	print 'Saving ' + REPORT_TXT
+	open(REPORT_TXT, 'w').write(reportText)
+
 def argsToSampleID(plateSet, plate, letter, column):
 	return plateSet+"%02d"%(plate,)+letter+"%02d"%(column)
 
@@ -104,17 +176,25 @@ def main():
 	directoryListing = listdir(os.path.dirname(os.path.abspath(__file__)) + '/' + ASSEMBLED_DIRECTORY)
 	fastQs = findAssembledFASTQ(directoryListing)
 
-	for i in range(len(fastQs)):
-		f = fastQs[i]
+	for fqIndex in range(len(fastQs)):
+		print "Filtering " + str(fastQs[fqIndex]) + '\t' + str(100.0*fqIndex/len(fastQs)) + '%' 
+		filterFastQ(fastQs[fqIndex])
+
+	directoryListing = listdir(os.path.dirname(os.path.abspath(__file__)) + '/' + FILTERED_DIRECTORY)
+	filteredFASTAs = findFilteredFASTA(directoryListing)
+
+	for i in range(len(filteredFASTAs)):
+		f = filteredFASTAs[i]
 		sample_name = f[:8]
 		print '\n' + time.strftime('%c') + '\n'
-		print 'PROGRESS: ' + str(i+1) + '/' + str(len(fastQs)) + '   ' + str((100.0*i)/len(fastQs)) + '%'
+		print 'PROGRESS: ' + str(i+1) + '/' + str(len(filteredFASTAs)) + '   ' + str((100.0*i)/len(filteredFASTAs)) + '%'
 		print 'Running USEARCH on ' + f
-		usearch(ASSEMBLED_DIRECTORY + '/' + f)
+		usearch(FILTERED_DIRECTORY + '/' + f)
 		print 'Tallying ' + f
 		tally(sample_name)
 	# print sample_dictionary['HMB01A01']
 	saveTallyText()
 	saveTallyHMB()
+	saveReport()
 
 main()
