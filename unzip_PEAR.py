ec8a512c3720c1a250e8588d2f3ca1608748c97a
Ryan Hsu <ryanusahk@gmail.com>
2015-11-25 16:22:38 -0800

    unzipPEAR navigates basespace file structure and unzips/PEAR merges all pair end reads.

--- unzip_PEAR.py
+++ unzip_PEAR.py
@@ -3,11 +3,17 @@ import os
 import gzip
 import sys
 from subprocess import call
+from os.path import expanduser
+import time
+import subprocess
 
 UNZIPPED_DIRECTORY = "unzipped/"
 ASSEMBLED_DIRECTORY = "assembled/"
 
 walk_dir = sys.argv[1]
+
+
+
 
 if not os.path.exists(UNZIPPED_DIRECTORY):
     os.makedirs(UNZIPPED_DIRECTORY)
@@ -15,53 +21,9 @@ if not os.path.exists(ASSEMBLED_DIRECTORY):
     os.makedirs(ASSEMBLED_DIRECTORY)
 
 
-for root, subdirs, files in os.walk(walk_dir):
-	if len(files) > 0:
-		for f in files:
-			if f.split('.')[-1] == 'gz':
-				print 'Copying/Unzipping ' + f
-				call(["cp", os.path.join(root, f), UNZIPPED_DIRECTORY])
-				call(["gunzip", UNZIPPED_DIRECTORY + f])
-
-# Create Log File
-fullLog = open('merge_report.log', 'w')
-fullLog.close()
-
-fastqFiles = []
-for f in UNZIPPED_DIRECTORY:
-	if f.split('.')[len(f.split('.'))-1] == 'fastq':
-		fastqFiles.append(f)
-
-forwardReads = []
-for f in fastqFiles:
-	if f.split('_')[3] == 'R1':
-		forwardReads.append(f)
-
-pairedReads = []
-for fwd in forwardReads:
-	ID = fwd.split('_')[:2]
-	for rev in fastqFiles:
-		if rev.split('_')[:2] == ID and rev.split('_')[3] == 'R2':
-			pairedReads.append([fwd, rev])
-			print [fwd, rev]
-
-
-print 'Found ' + str(len(pairedReads)) + ' pairs'
-
-print 'Please check all pairs are accounted for!'
-
-print 'PEAR EVERYTHING'
-response = raw_input('Continue? y/n: ')
-if response == 'y':
-	for pair in pairedReads:
-		runPear(pair)
-else:
-	exit()
-
-
 def runPear(pair):
 	home = expanduser("~")
-	args = [home + '/bin/pear', '-f', unzippedDirectory + '/' + pair[0], '-r', unzippedDirectory + '/' + pair[1], '-o', pair[0].split('_')[0], '-j', '8']
+	args = [home + '/bin/pear', '-f', UNZIPPED_DIRECTORY + pair[0], '-r', UNZIPPED_DIRECTORY + pair[1], '-o', ASSEMBLED_DIRECTORY + pair[0].split('_')[0], '-j', '8']
 
 	print time.strftime('%c') + '\n'
 	print 'Running PEAR on ' + str(pair) + '\n'
@@ -77,3 +39,48 @@ def runPear(pair):
 	fullLog = open('merge_report.log', 'a')
 	fullLog.write('\n')
 	fullLog.close()
+
+# for root, subdirs, files in os.walk(walk_dir):
+# 	if len(files) > 0:
+# 		for f in files:
+# 			if f.split('.')[-1] == 'gz':
+# 				print 'Copying/Unzipping ' + f
+# 				call(["cp", os.path.join(root, f), UNZIPPED_DIRECTORY])
+# 				call(["gunzip", UNZIPPED_DIRECTORY + f])
+
+# Create Log File
+fullLog = open('merge_report.log', 'w')
+fullLog.close()
+
+fastqFiles = []
+for f in listdir(UNZIPPED_DIRECTORY):
+	if f.split('.')[len(f.split('.'))-1] == 'fastq':
+		fastqFiles.append(f)
+
+forwardReads = []
+for f in fastqFiles:
+	if f.split('_')[3] == 'R1':
+		forwardReads.append(f)
+forwardReads = sorted(forwardReads)
+
+pairedReads = []
+for fwd in forwardReads:
+	ID = fwd.split('_')[:2]
+	for rev in fastqFiles:
+		if rev.split('_')[:2] == ID and rev.split('_')[3] == 'R2':
+			pairedReads.append([fwd, rev])
+
+
+print 'Found ' + str(len(pairedReads)) + ' pairs'
+for pair in pairedReads:
+	runPear(pair)
+
+
+
+
+
+
+
+
+
+
